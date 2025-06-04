876f157e30b5ae89e8e42cb214be3bb47a33357f
Ingrid Pérez <86657952+ingridpi@users.noreply.github.com>
2025-06-04 15:07:13 +0100

    Add Configuration (#1)

---
+++ config/config.py
@@ -0,0 +1,8 @@
+START_DATE = "2016-01-01"
+END_DATE = "2025-01-01"
+
+DATA_DIR = "data"
+PLOT_DIR = "figures"
+
+EXCHANGE_NYSE = "XNYS"  # New York Stock Exchange
+EXCHANGE_LSE = "XLON"  # London Stock Exchange
