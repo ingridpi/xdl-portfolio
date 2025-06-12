f9cb37dd4e8ddbcb7cc9bf60f692c1ba7fa567be
Ingrid Pérez <99.perez.in@gmail.com>
2025-06-12 10:30:56 +0100

    Add plot close prices method

---
+++ visualiser/findata_visualiser.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+class FinancialDataVisualiser:
+    def __init__(self) -> None:
+        pass
+
+    def plot_close_prices(self, data: pd.DataFrame) -> None:
+
+        # Sample the first 10 tickers if there are more than 10 unique tickers
+        if data.Ticker.nunique() > 10:
+            sample_tickers = data["Ticker"].unique()[:10]
+            data = data[data["Ticker"].isin(sample_tickers)]
+
+        plt.figure(figsize=(12, 6))
+        sns.lineplot(data=data, x="Date", y="Close", hue="Ticker")
+        plt.title("Closing Prices of Tickers")
+        plt.ylabel("Closing Price")
+        plt.show()
+
