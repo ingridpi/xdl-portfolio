fc6b1b7f54a0f2d44f6a191c5605ba29733c3431
Ingrid Pérez <99.perez.in@gmail.com>
2025-06-12 10:16:32 +0100

    add asset group names

--- config/config.py
+++ config/config.py
@@ -4,5 +4,13 @@ END_DATE = "2025-01-01"
 DATA_DIR = "data"
 PLOT_DIR = "figures"
 
+TEST_NAME = "test"
+DOW_30_NAME = "dow30"
+EURO_STOXX_50_NAME = "eurostoxx50"
+FTSE_100_NAME = "ftse100"
+SP_500_NAME = "sp500"
+FX_NAME = "currencies"
+COMMODITY_NAME = "futures"
+
 EXCHANGE_NYSE = "XNYS"  # New York Stock Exchange
 EXCHANGE_LSE = "XLON"  # London Stock Exchange
