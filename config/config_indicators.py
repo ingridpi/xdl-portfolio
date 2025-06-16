734406b81e9cc847e4f1f31b184c3b7eed440d38
Ingrid Pérez <99.perez.in@gmail.com>
2025-06-16 14:29:21 +0100

    Add technical indicators and macroeconomic indicators

---
+++ config/config_indicators.py
@@ -0,0 +1,24 @@
+# List of technical indicators
+TECHNICAL_INDICATORS = [
+    "close_5_sma",  # Simple Moving Average over 5 days (1 week)
+    "close_10_sma",  # Simple Moving Average over 10 days (2 weeks)
+    "close_20_sma",  # Simple Moving Average over 20 days (1 month)
+    "close_5_ema",  # Exponential Moving Average over 5 days (1 week)
+    "close_10_ema",  # Exponential Moving Average over 10 days (2 weeks)
+    "macd",  # Moving Average Convergence Divergence
+    "rsi",  # Relative Strength Index over 14 days
+    "kdjk",  # KDJ indicator - K% line
+    "kdjd",  # KDJ indicator - D% line
+    "cci",  # Commodity Channel Index
+    "boll",  # Bollinger Bands
+    "boll_ub",  # Bollinger Bands Upper Band
+    "boll_lb",  # Bollinger Bands Lower Band
+    "atr",  # Average True Range
+    "adx",  # Average Directional Index
+    "vwma",  # Volume Weighted Moving Average
+]
+
+# List of macroeconomic indicators
+MACROECONOMIC_INDICATORS = [
+    "^VIX",  # Volatility Index (VIX)
+]
