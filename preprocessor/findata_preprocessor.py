e3193774382c0e37b43c401d7923591ff644871b
Ingrid Pérez <99.perez.in@gmail.com>
2025-06-16 14:39:41 +0100

    Add technical indicators preprocessing

--- preprocessor/findata_preprocessor.py
+++ preprocessor/findata_preprocessor.py
@@ -3,6 +3,7 @@ import itertools
 import pandas as pd
 from typing import List
 import exchange_calendars as ecals
+from stockstats import StockDataFrame
 
 
 class FinancialDataPreprocessor:
@@ -28,6 +29,9 @@ class FinancialDataPreprocessor:
 
         # Add day of the week column
         df["DayOfWeek"] = df["Date"].dt.dayofweek
+
+        if use_tech_indicators:
+            df = self.__add_technical_indicators(df, tech_indicators)
 
         return df.reset_index(drop=True)
 
@@ -96,3 +100,44 @@ class FinancialDataPreprocessor:
         test_data = data[data["Date"] > train_end_date].copy()
 
         return train_data, test_data
+
+    def __add_technical_indicators(
+        self, data: pd.DataFrame, indicators: List[str]
+    ) -> pd.DataFrame:
+        """
+        Add technical indicators to the DataFrame based on the specified indicators.
+        """
+
+        # Convert the dataframe to StockDataFrame
+        stock_df = StockDataFrame.retype(data.copy())
+        tickers = data["Ticker"].unique()
+
+        # Iterate over the indicators
+        for indicator in indicators:
+            indicator_df = pd.DataFrame()
+
+            # Iterate over each ticker
+            for ticker in tickers:
+
+                # Extract the indicator data for the ticker
+                try:
+                    ind_df = stock_df[stock_df["ticker"] == ticker][indicator]
+                    ind_df = pd.DataFrame(ind_df)
+                    ind_df["Ticker"] = ticker
+                    ind_df["Date"] = data[data["Ticker"] == ticker][
+                        "Date"
+                    ].values
+
+                    indicator_df = pd.concat(
+                        [indicator_df, ind_df], ignore_index=True
+                    )
+                except Exception as e:
+                    print(
+                        f"Error processing indicator '{indicator}' for ticker '{ticker}': {e}"
+                    )
+
+            data = data.merge(indicator_df, on=["Ticker", "Date"], how="left")
+
+        data.fillna(0, inplace=True)  # Fill NaN values with 0
+
+        return data
