e8064fc8091f4225beaaa36c57d4cee7bab3bb20
Ingrid Pérez <99.perez.in@gmail.com>
2025-06-24 12:01:42 +0100

    Fix imports

--- visualiser/__init__.py
+++ visualiser/__init__.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
 import os
+
+import matplotlib.pyplot as plt
 
 # Use custom matplotlib style
 style = os.path.dirname(os.path.abspath(__file__)) + "/style.mplstyle"
