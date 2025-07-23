2bf00f2028ab8a68bb717e7e0bd71b0fd06553c5
Ingrid Pérez <99.perez.in@gmail.com>
2025-07-23 14:13:58 +0100

    Add SHAP Explainability visualisations

---
+++ visualiser/shap_visualiser.py
@@ -0,0 +1,201 @@
+from re import S
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import shap
+
+shap.initjs()
+
+
+class ShapVisualiser:
+    def __init__(
+        self,
+        shap_values: shap.Explanation,
+        shap_interaction_values: np.ndarray,
+        action_space: pd.DataFrame,
+        X_test: pd.DataFrame,
+    ) -> None:
+        """
+        Initializes the SHAP visualiser with SHAP values and action space.
+        :param shap_values: SHAP explanation object containing the SHAP values.
+        :param shap_interaction_values: SHAP interaction values for the features.
+        :param action_space: DataFrame containing the action space columns.
+        :param X_test: DataFrame containing the test state space data.
+        """
+        self.shap_values = shap_values
+        self.shap_interaction_values = shap_interaction_values
+        self.action_space = action_space
+        self.X_test = X_test
+
+    def beeswarm_plot(
+        self,
+        index: int,
+        directory: str,
+        filename: str,
+    ) -> None:
+        """
+        Create a beeswarm plot for the SHAP values.
+        :param index: Index of the asset in the action space to plot.
+        :param directory: Directory where the plot will be saved.
+        :param filename: Base filename for the saved plot.
+        :return: None
+        """
+        ax = shap.plots.beeswarm(self.shap_values[..., index], show=False)
+        asset = self.action_space.columns[index]
+        ax.set_title(f"SHAP Beeswarm Plot for {asset}")
+        plt.savefig(f"{directory}/{filename}_shap_beeswarm_{asset}.png")
+        plt.show()
+
+    def force_plot(
+        self,
+        index: int,
+        directory: str,
+        filename: str,
+    ) -> None:
+        """
+        Create a force plot for the SHAP values.
+        :param index: Index of the asset in the action space to plot.
+        :param directory: Directory where the plot will be saved.
+        :param filename: Base filename for the saved plot.
+        :return: None
+        """
+        shap.plots.force(
+            self.shap_values[..., index],
+            feature_names=self.X_test.columns,
+            link="logit",
+            show=False,
+        )
+        plt.show()
+
+        shap.save_html(
+            f"{directory}/{filename}_shap_force_{self.action_space.columns[index]}.html",
+            shap.plots.force(
+                self.shap_values[..., index],
+                feature_names=self.X_test.columns,
+                link="logit",
+            ),
+        )
+
+    def force_plot_single_obs(
+        self,
+        index: int,
+        obs: int,
+        directory: str,
+        filename: str,
+    ) -> None:
+        """
+        Create a force plot for a single observation.
+        :param index: Index of the asset in the action space to plot.
+        :param obs: Index of the observation to plot in time.
+        :param directory: Directory where the plot will be saved.
+        :param filename: Base filename for the saved plot.
+        :return: None
+        """
+        shap.plots.force(
+            self.shap_values[obs, ..., index],
+            feature_names=self.X_test.columns,
+            link="logit",
+            matplotlib=True,
+            show=False,
+        )
+        asset = self.action_space.columns[index]
+
+        plt.title(
+            f"SHAP Force Plot for {asset} for observation {obs}",
+            fontsize=16,
+            y=1.5,
+        )
+
+        # Format value labels to two decimal places
+        for text in plt.gca().texts:
+            value = text.get_text()
+            # If value contains = sign, format the number
+            if "=" in value:
+                parts = value.split("=")
+                if len(parts) == 2:
+                    try:
+                        number = float(parts[1])
+                        text.set_text(f"{parts[0]}= {number:.3f}")
+                    except ValueError:
+                        pass
+
+        plt.savefig(
+            f"{directory}/{filename}_shap_force_single_obs_{asset}_{obs}.png"
+        )
+        plt.show()
+
+    def waterfall_plot_single_obs(
+        self,
+        index: int,
+        obs: int,
+        directory: str,
+        filename: str,
+    ) -> None:
+        """
+        Create a waterfall plot for the SHAP values.
+        :param index: Index of the asset in the action space to plot.
+        :param obs: Index of the observation to plot in time.
+        :param directory: Directory where the plot will be saved.
+        :param filename: Base filename for the saved plot.
+        :return: None
+        """
+        shap.plots.waterfall(
+            self.shap_values[obs, ..., index],
+            show=False,
+        )
+
+        asset = self.action_space.columns[index]
+
+        plt.title(f"SHAP Waterfall Plot for {asset}")
+        plt.savefig(f"{directory}/{filename}_shap_waterfall_{asset}.png")
+        plt.show()
+
+    def heatmap(
+        self,
+        index: int,
+        directory: str,
+        filename: str,
+    ) -> None:
+        """
+        Create a heatmap for the SHAP values.
+        :param index: Index of the asset in the action space to plot.
+        :param directory: Directory where the plot will be saved.
+        :param filename: Base filename for the saved plot.
+        :return: None
+        """
+        shap.plots.heatmap(
+            self.shap_values[..., index],
+            max_display=10,
+            show=False,
+        )
+        asset = self.action_space.columns[index]
+        plt.title(f"SHAP Heatmap for {asset}")
+        plt.savefig(f"{directory}/{filename}_shap_heatmap_{asset}.png")
+        plt.show()
+
+    def interaction_plot(
+        self,
+        index: int,
+        directory: str,
+        filename: str,
+    ) -> None:
+        """
+        Create a summary plot for the SHAP interaction values.
+        :param index: Index of the asset in the action space to plot.
+        :param directory: Directory where the plot will be saved.
+        :param filename: Base filename for the saved plot.
+        :return: None
+        """
+        shap.summary_plot(
+            self.shap_interaction_values[..., index], self.X_test, show=False
+        )
+
+        asset = self.action_space.columns[index]
+        plt.suptitle(
+            f"SHAP Interaction Values for {asset}",
+            y=1.1,
+            fontsize=14,
+        )
+        plt.savefig(f"{directory}/{filename}_shap_interaction_{asset}.png")
+        plt.show()
