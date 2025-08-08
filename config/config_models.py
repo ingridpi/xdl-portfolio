5d8fdb75ee6a8938db65d231025a4b708b617d1e
Ingrid Pérez <99.perez.in@gmail.com>
2025-08-08 11:44:03 +0100

    Update sweep model config

--- config/config_models.py
+++ config/config_models.py
@@ -151,59 +151,31 @@ train_visualisation_config = {
 # Model hyperparameter sweep configuration
 MODEL_SWEEP_CONFIG = {
     "a2c": {
-        "n_steps": {"values": [5, 10, 20, 30, 40]},
-        "ent_coef": {
-            "distribution": "log_uniform",
-            "min": 1e-8,
-            "max": 1,
-        },
-        "learning_rate": {
-            "distribution": "log_uniform",
-            "min": 1e-5,
-            "max": 1e-2,
-        },
+        "n_steps": [5, 10, 20, 30, 40],
+        "ent_coef": [1e-8, 1e-6, 1e-4, 1e-2, 0.1, 0.5, 1],
+        "learning_rate": [1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2],
     },
     "ppo": {
-        "n_steps": {"values": [128, 256, 512, 1024, 2048]},
-        "ent_coef": {
-            "distribution": "log_uniform",
-            "min": 1e-8,
-            "max": 1,
-        },
-        "learning_rate": {
-            "distribution": "log_uniform",
-            "min": 1e-5,
-            "max": 1e-2,
-        },
-        "batch_size": {"values": [32, 64, 128, 256, 512]},
+        "n_steps": [128, 256, 512, 1024, 2048],
+        "ent_coef": [1e-8, 1e-6, 1e-4, 1e-2, 0.1, 0.5, 1],
+        "learning_rate": [1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2],
+        "batch_size": [32, 64, 128, 256, 512],
     },
     "ddpg": {
-        "batch_size": {"values": [64, 128, 256]},
-        "buffer_size": {"values": [50000, 100000, 200000, 500000]},
-        "learning_rate": {
-            "distribution": "log_uniform",
-            "min": 1e-5,
-            "max": 1e-2,
-        },
+        "batch_size": [64, 128, 256],
+        "buffer_size": [50000, 100000, 200000, 500000],
+        "learning_rate": [1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2],
     },
     "td3": {
-        "batch_size": {"values": [64, 100, 128, 256]},
-        "buffer_size": {"values": [500000, 1000000, 2000000]},
-        "learning_rate": {
-            "distribution": "log_uniform",
-            "min": 1e-5,
-            "max": 1e-2,
-        },
+        "batch_size": [64, 100, 128, 256],
+        "buffer_size": [500000, 1000000],
+        "learning_rate": [1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2],
     },
     "sac": {
-        "batch_size": {"values": [32, 64, 128]},
-        "buffer_size": {"values": [100000, 500000, 1000000, 2000000]},
-        "learning_rate": {
-            "distribution": "log_uniform",
-            "min": 1e-5,
-            "max": 1e-2,
-        },
-        "learning_starts": {"values": [500, 1000, 2000, 5000]},
-        "ent_coef": {"values": ["auto", "auto_0.1", "auto_0.01"]},
+        "batch_size": [32, 64, 128],
+        "buffer_size": [100000, 500000, 1000000],
+        "learning_rate": [1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2],
+        "learning_starts": [500, 1000, 2000, 5000],
+        "ent_coef": ["auto", "auto_0.1", "auto_0.01"],
     },
 }
